3c4176eee948e4b1594f05943f49a88b9334e842
Nyasa <nyasa@gmail.com>
2025-12-02 21:02:21 +0530

    part 2

--- 2025/python/day2.py
+++ 2025/python/day2.py
@@ -1,3 +1,29 @@
+# total = 0
+
+# with open("inputday2.txt") as f:
+#     line = f.read().strip()
+
+# ranges = line.split(",")
+
+# def is_invalid_id(n):
+#     s = str(n)
+#     if len(s) % 2 != 0:
+#         return False
+#     if s[0] == '0':
+#         return False
+
+#     mid = len(s) // 2
+#     return s[:mid] == s[mid:]
+
+# for r in ranges:
+#     start, end = map(int, r.split("-"))
+#     for num in range(start, end + 1):
+#         if is_invalid_id(num):
+#             total += num
+
+# print(total)
+
+
 total = 0
 
 with open("inputday2.txt") as f:
@@ -5,20 +31,28 @@ with open("inputday2.txt") as f:
 
 ranges = line.split(",")
 
-def is_invalid_id(n):
+def is_invalid_id_part2(n):
     s = str(n)
-    if len(s) % 2 != 0:
-        return False
+    L = len(s)
+
     if s[0] == '0':
         return False
 
-    mid = len(s) // 2
-    return s[:mid] == s[mid:]
+    for k in range(1, L // 2 + 1):
+        if L % k != 0:
+            continue
+        r = L // k
+        if r < 2:
+            continue
+        if s == s[:k] * r:
+            return True
+
+    return False
 
 for r in ranges:
     start, end = map(int, r.split("-"))
     for num in range(start, end + 1):
-        if is_invalid_id(num):
+        if is_invalid_id_part2(num):
             total += num
 
 print(total)
