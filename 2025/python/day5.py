f5d490aa51f559b8a1695af4ab8a95cf45c54db3
Nyasa <nyasa@gmail.com>
2025-12-06 18:57:02 +0530

    day 5 part 2

--- 2025/python/day5.py
+++ 2025/python/day5.py
@@ -1,5 +1,37 @@
+# ranges = []
+# ids = []
+
+# with open("inputday5.txt") as f:
+#     lines = [line.strip() for line in f]
+
+# i = 0
+
+# while lines[i] != "":
+#     a, b = lines[i].split("-")
+#     ranges.append((int(a), int(b)))
+#     i += 1
+
+# i += 1
+
+# while i < len(lines) and lines[i] != "":
+#     ids.append(int(lines[i]))
+#     i += 1
+
+# fresh = 0
+
+# for x in ids:
+#     ok = False
+#     for a, b in ranges:
+#         if a <= x <= b:
+#             ok = True
+#             break
+#     if ok:
+#         fresh += 1
+
+# print(fresh)
+
+
 ranges = []
-ids = []
 
 with open("inputday5.txt") as f:
     lines = [line.strip() for line in f]
@@ -11,21 +43,22 @@ while lines[i] != "":
     ranges.append((int(a), int(b)))
     i += 1
 
-i += 1
+ranges.sort()
 
-while i < len(lines) and lines[i] != "":
-    ids.append(int(lines[i]))
-    i += 1
+merged = []
+start, end = ranges[0]
 
-fresh = 0
+for a, b in ranges[1:]:
+    if a <= end + 1:       
+        end = max(end, b)   
+    else:
+        merged.append((start, end))
+        start, end = a, b
 
-for x in ids:
-    ok = False
-    for a, b in ranges:
-        if a <= x <= b:
-            ok = True
-            break
-    if ok:
-        fresh += 1
+merged.append((start, end))
 
-print(fresh)
+total = 0
+for a, b in merged:
+    total += (b - a + 1)
+
+print(total)
